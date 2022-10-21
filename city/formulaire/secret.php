1cb86c8e6b53dab7da5f05182fdc33af71c8c4e2
Léa <nguyen.lea@hotmail.fr>
2022-10-22 00:25:09 +0200

    fix/links

--- city/formulaire/secret.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = "sqletud.u-pem.fr";
-$dbname = "lnguye23_db";
-$username = "lnguye23";
-$password = "motdepasse";
-?>
--- room/index.php
+++ room/index.php
@@ -19,7 +19,7 @@
 
         <?php
 
-        include('secret.php');
+        include('../secret.php');
 
         $link = new PDO("mysql:host={$host};dbname={$dbname};", $username, $password, array (PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         
